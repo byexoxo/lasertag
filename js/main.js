23c9ccb2f5841bce4c3ea3c3f91e9370021c1fe0
JJ <jedi.dookun@outlook.com>
2019-09-06 15:07:42 -0700

    Updated input

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ let score = document.querySelector("#score");
 let input = document.querySelector("#input");
 let button = document.querySelector("#button");
 let count = 0;
+let length = 0;
 
 document.addEventListener("click", () => input.focus());
 
 input.addEventListener("input", () => {
-  input.value = "";
-  count++;
+  length = input.value.length;
+  if (length > 1) count++;
   score.innerHTML = count;
+  input.value = "";
 });
 
 button.addEventListener("click", () => {
